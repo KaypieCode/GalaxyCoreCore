0dde49e506968e42fd9722ba07a459ffb75fae24
Bloeckchengrafik <37768199+Bloeckchengrafik@users.noreply.github.com>
2021-08-03 21:51:19 +0200

    Revert "Dev I18N Merging"

--- src/main/java/net/galaxycore/galaxycorecore/permissions/exceptions/MissingFieldException.java
+++
@@ -1,7 +0,0 @@
-package net.galaxycore.galaxycorecore.permissions.exceptions;
-
-public class MissingFieldException extends RuntimeException{
-    public MissingFieldException(String s) {
-        super("Missing Field: " + s);
-    }
-}

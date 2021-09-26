73f48416cd06a9d0f91654227626a8f7e30cdbe3
Florian Sandmann <72268408+Flo-Mit-H@users.noreply.github.com>
2021-09-26 19:09:19 +0200

    Update core.create.mysql.sql

--- src/main/resources/sql/core.create.mysql.sql
+++ src/main/resources/sql/core.create.mysql.sql
@@ -32,6 +32,18 @@ CREATE TABLE IF NOT EXISTS `core_bans`
     `staff`     INTEGER
 );
 
+CREATE TABLE IF NOT EXISTS `core_banlog`
+(
+    `id`        INTEGER AUTO_INCREMENT PRIMARY KEY,
+    `userid`    INTEGER,
+    `reasonid`  INTEGER,
+    `from`      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `until`     TIMESTAMP,
+    `permanent` BOOLEAN DEFAULT false,
+    `staff`     INTEGER,
+    `date`      TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
 CREATE TABLE IF NOT EXISTS `core_mutes`
 (
     `id`         INTEGER AUTO_INCREMENT PRIMARY KEY,
